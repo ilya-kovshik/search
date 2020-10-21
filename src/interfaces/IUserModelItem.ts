659a8f3d38b26d95902cc5ef0a9c388ea79c8ef9
Ilya Kovshik <lopodrok@gmail.com>
2020-10-21 21:57:36 +0300

    [feat]: UserModel updates

--- src/interfaces/IUserModelItem.ts
+++ src/interfaces/IUserModelItem.ts
@@ -1,23 +1,23 @@
 export interface IUserModelItem {
-    "id": string;
-    "name": string;
-    "username": string;
-    "email": string;
-    "address": {
-      "street": string;
-      "suite": string;
-      "city": string;
-      "zipcode": string;
-      "geo": {
-        "lat": string;
-        "lng": string
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+      street: string;
+      suite: string;
+      city: string;
+      zipcode: string;
+      geo: {
+        lat: string;
+        lng: string;
       }
-    },
-    "phone": string;
-    "website": string;
-    "company": {
-      "name": string;
-      "catchPhrase": string;
-      "bs": string
-    }
+    };
+    phone: string;
+    website: string;
+    company: {
+      name: string;
+      catchPhrase: string;
+      bs: string
+    };
 }
--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -2,8 +2,12 @@ import { serverConfig } from "../configs/serverConfig";
 import { IUserModelItem } from "../interfaces/IUserModelItem";
 import { IUsersModel } from "../interfaces/IUsersModel";
 
-class UsersModel implements IUsersModel {
+export class UsersModel implements IUsersModel {
     private static _instance: UsersModel;
+    private hash: {isLoaded: boolean, data: IUserModelItem[]} = {
+        isLoaded: false,
+        data: []
+    };
 
     public getInstance(): UsersModel {
         if (!UsersModel._instance) {
@@ -15,13 +19,20 @@ class UsersModel implements IUsersModel {
 
     public loadUsers(): Promise<IUserModelItem[]> {
         return fetch(`${serverConfig.baseUrl}/${serverConfig.users}`)
-            .then(res => res.json());
+            .then(res => res.json())
+            .then((data: IUserModelItem[]) => {
+                this.hash.isLoaded = true;
+                this.hash.data.push(...data);
+
+                return data;
+            });
+    }
+
+    public getUsers(): Promise<IUserModelItem[]>  {
+        if(!this.hash.isLoaded) {
+            return this.loadUsers();
+        }
+
+        return new Promise((res) => res(this.hash.data));
     }
 }
-
-const usersModel: UsersModel = new UsersModel();
-
-export {
-    usersModel,
-    UsersModel
-};
